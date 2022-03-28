3581dac5fd33c2f97906e78f31c0a6c2542b844b
AbigailMetanoia <100354595+AbigailMetanoia@users.noreply.github.com>
2022-03-28 09:44:42 +0700

    Update SelisihKetinggian.c

--- Praktikum 5/SelisihKetinggian.c
+++ Praktikum 5/SelisihKetinggian.c
@@ -7,34 +7,25 @@
 
 #include <stdio.h>
 
-int main (){
 	//Kamus
-	int i,j,T,N, jarak, terKecil, terBesar;
+	int i,N, jarak, terKecil, terBesar;
 	
 	//Algoritma
 	printf("Membuat Program Selisih Ketinggian \n");
-	printf("Masukan nilai N : ");
 	scanf("%d", &N);
+    	int T[N];
+    
+    for (i = 0; i < N; i++) scanf("%d", &T[i]);
+
+    terBesar = T[0];
+    terKecil = T[0];
+
+    for (i = 0; i < N; i++) if (T[i] > terBesar) terBesar = T[i];
+
+    for (i = 0; i < N; i++) if (T[i] < terKecil) terKecil = T[i];
 	
-	
-	
-	for(i = 1; i <= N; i++){
-			int T[i];
-			terKecil = T[0];
-			terBesar = T[0];
-		
-	}
-	for(j = 1; j <= N; j++){
-			int T[j];
-			if (T[j] >= terBesar){
-				terBesar = T[j];
-			}
-			else if (T[j] >= terKecil){
-				terKecil = T[j];
-			}		
-	}
-	jarak = terBesar - terKecil;
-	printf("%d", jarak);
-	
-	return 0;
+    jarak = terBesar-terKecil;
+    printf("%d", jarak);
+    
+    return 0;
 }
